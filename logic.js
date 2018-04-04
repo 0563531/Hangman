7dfc31185ccb6125d9e23bc2f78b12f2aeb18ce7
Chevalier <0563531@students.dadeschools.net>
2018-04-04 12:09:38 -0400

    hhhhhh

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-var worldList = ['Hitler', 'Napoleon', 'Columbus'];
+var worldList = ['Audi', 'Ferrari', 'Tesla'];
 
 var chosenword = '';
 
@@ -6,7 +6,7 @@ var lettersInChosenWord = [];
 
 var numBlanks = 0;
 
-var blanksAmdSuccesses = [];
+var blanksAndSuccesses = [];
 
 var wrongGuesses = [];
 
@@ -28,12 +28,87 @@ function startGame() {
 
 	numGuesses = 9;
 
-	chosenword = worldList[math.floor(math.random() = fruits.length)];
+	chosenword = worldList[Math.floor(Math.random() = worldList.length)];
 
-	lettersInChosenWord = chosenword.split('');
+	lettersInChosenWord = chosenword.split("");
 
 	numBlanks = lettersInChosenWord.length;
 
 	console.log(chosenword)
 
+	blanksAndSuccesses = [];
+
+	wrongGuesses = [];
+
+	for (var 1 =0; i< numBlanks; i++) {
+		blanksAndSuccesses.push("_");
+	}
+
+	console.log(blanksAndSuccesses);
+
+	document.getElementById('guesses-left').innerHTML = numGuesses;
+
+	document.getElementById('word-blanks').innerHTML = blanksAndSuccesses.join(" ");
+
+	document.getElementById('wrong-guesses').innerHTML = wrongGuesses.join(" ");
+}
+
+
+function checkLetters(Letter) {
+	for (var i = 0; i< numBlanks; i++) {
+		if (chosenword[i] === letter) {
+			letterInWord = true;
+		}
+	}
+
+	if (letterInWord) {
+		for (var j = 0; j < numBlanks; j++) {
+			if (chosenword[j] === letter) {
+				blanksAndSuccesses[j] = letter;
+			}
+		}
+		console.log(blanksAndSuccesses);
+	}
+
+	else {
+		wrongGuesses.push(letter);
+		numGuesses--;
+	}
+}
+
+function roundComplete() {
+	console.log("Win count: " + winCounter + " | Loss count: " + lossCounter + " | numGuesses: " + numGuesses);
+
+	document.getElementById('guesses-left').innerHTML = numGuesses;
+
+	document.getElementById('word-blanks').innerHTML = blanksAndSuccesses.join(" ");
+
+	document.getElementById('wrong-guesses').innerHTML = wrongGuesses.join(" ");
+
+	if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+		winCounter++;
+		alert("You win!");
+
+		document.getElementById("win-counter").innerHTML = winCounter;
+
+		startGame();
+	}
+
+	else if (numGuesses === 0) {
+		lossCounter++;
+		alert("You lost")
+
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+
+		startGame();
+	}
+}
+
+startGame();
+
+
+document.onkeyup = function(event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed);
+	roundComplete();
 }
